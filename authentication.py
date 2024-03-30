4bafd8555ae461a80a7df043b4895b4bea7b3cca
UĞUR GÜMÜŞ <83755769+agrogur@users.noreply.github.com>
2024-03-30 03:04:43 +0300

    Depersonalization Update authentication.py

--- authentication.py
+++ authentication.py
@@ -1,21 +1,21 @@
 import pyrebase
 
 config = {
-  'apiKey': "AIzaSyDIWINaH8Q7cBshxqzDcHphJKLWvRRELQc",
-  'authDomain': "blissful-cell-393422.firebaseapp.com",
-  'projectId': "blissful-cell-393422",
-  'storageBucket': "blissful-cell-393422.appspot.com",
-  'messagingSenderId': "208919849448",
-  'appId': "1:208919849448:web:f03dd4ad2812f3b81f5c5d",
-  'measurementId': "G-D9FPTHTPYW",
+  'apiKey': "[USE YOUR OWN API KEY]",
+  'authDomain': "...",
+  'projectId': "...",
+  'storageBucket': "...",
+  'messagingSenderId': "...",
+  'appId': "...",
+  'measurementId': "...",
   'databaseURL': " "
 }
 
 firebase = pyrebase.initialize_app(config)
 auth = firebase.auth()
 
-email = 'test@gmail.com'
-password = 'test1234567'
+email = ''
+password = ''
 
 
 #user = auth.create_user_with_email_and_password(email, password)
